35f15b3049b92d1e7f586903967abb25b99c352c
T11x <T11x11@gmail.com>
2023-04-05 21:50:07 +0300

    добавляет сверстанные блоки

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {Burger} from './utils/burger';
 
 // ---------------------------------
 
@@ -13,6 +14,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modules
   // ---------------------------------
+
+  const burger = new Burger();
+  burger.init();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
